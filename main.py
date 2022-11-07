e145bbe98edb26562c36930b16ae97ed465b0316
reignofshadovv <mehmetsatici3@gmail.com>
2022-11-07 22:30:49 +0300

    Made a Welcome banner.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("HelloWorld")
+#Project Start Date : "07.11.2022-22:00"
+#MyPythonProject1
+
+banner1 = ("=============================================================")
+banner2 = ("==========      HELLO & WELCOME TO MY PROJECT      ==========")
+banner3 = ("=============================================================")
+print(banner1)
+print(banner2)
+print(banner3)
