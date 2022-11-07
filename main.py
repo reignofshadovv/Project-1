0082083f7a6a14743d4c57cf0b26db3b625740be
reignofshadovv <mehmetsatici3@gmail.com>
2022-11-07 22:53:52 +0300

    Koin girdi aracı eklendi

--- main.py
+++ main.py
@@ -4,6 +4,22 @@
 banner1 = ("=============================================================")
 banner2 = ("==========      HELLO & WELCOME TO MY PROJECT      ==========")
 banner3 = ("=============================================================")
+#print(banner1)
+#print(banner2)
+#print(banner3)
+
+
+# Koin Alım & Satım Girişi
+title1="Koin Alım&Satım Girişi"
 print(banner1)
-print(banner2)
+print(title1)
 print(banner3)
+
+x = input("Alınan Koin Cinsi Gir...\n")
+y = input("Alınan Koin Miktarı Gir...\n")
+z = input("Toplam Ödenen USDT Gir...\n")
+
+result = float(z) / float(y)
+print("Alış Kuru")
+print("----------------")
+print(result, "USDT")
